fbb27b83815b701cf824d88bf04178c0cdb893d4
Jimin Kim <ultimate1994@naver.com>
2022-01-26 19:48:32 +0900

    CHEM-1 켐솔루션 프로젝트 시작 proxy 설정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
 
 function App() {
-  const [message, setMessage] = useState([]);
-  
-  useEffect(() => {
-    fetch("/")
-      .then((response) => {
-        return response.json();
-      }) 
-      .then(function (data) {
-        setMessage(data);
-      });
-  }, []);
-    
   return (
     <div className="App">
       <header className="App-header">
@@ -31,9 +18,6 @@ function App() {
         > 
         Learn React
         </a>
-        <ul>
-          {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)} 
-        </ul> 
       </header>
     </div>
   );
