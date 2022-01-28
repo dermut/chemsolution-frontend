0c2f44253e2ea48389027254ef3d6f2560e5630a
Jimin Kim <ultimate1994@naver.com>
2022-01-28 20:26:51 +0900

    CHEM-2 소개페이지 구성 유저 로고 추가

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,15 @@
 import logo from '../images/main_logo.png';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PersonAddAltRoundedIcon from '@mui/icons-material/PersonAddAltRounded';import { Link } from '@mui/material';
 
 function Header() {
     return (
         <header>
             <a href="/"><img src={logo} alt='main_logo'></img></a>
+            <AccountCircleIcon></AccountCircleIcon>
+            <Link href="/api/user/add">
+                <PersonAddAltRoundedIcon color="primary"></PersonAddAltRoundedIcon>
+            </Link>
         </header>
     );
 }
