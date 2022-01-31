6b2339c4010effc96495e3f382ba2d9880778c9d
Jimin Kim <ultimate1994@naver.com>
2022-01-31 16:59:32 +0900

    CHEM-2 소개페이지 구성 푸터 페이지 구성

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,12 @@
+import layout from '../css/layout.css';
+
 function Footer() {
     return (
-        <div>
-            Company Name : ChemSolution Co., Ltd.
-            Address : 71-5, Haeryongsandan 5-ro, Haeryong-myeon, Suncheon-si, Jeollanam-do, Republic of Korea
-            Tel : +82-61-726-8815 Fax : +82-61-725-8814
-            Copyright © 2017 chemsolution Co., Ltd All rights reserved.
+        <div className="footer_wrap">
+            <p className='footer_p'>Company Name : ChemSolution Co., Ltd.</p>
+            <p className='footer_p'>Address : 71-5, Haeryongsandan 5-ro, Haeryong-myeon, Suncheon-si, Jeollanam-do, Republic of Korea</p>
+            <p className='footer_p'>Tel : +82-61-726-8815 Fax : +82-61-725-8814</p>
+            <p className='footer_copyright'>Copyright © 2017 chemsolution Co., Ltd All rights reserved.</p>
         </div>
     );
 }
