c8a562d104d73b4ec5d37bdddd177da08f9c0893
Jimin Kim <ultimate1994@naver.com>
2022-02-01 08:54:52 +0900

    CHEM-2 소개페이지 구성 메인소개 페이지 구성 중

--- src/layout/IntroduceCorp.jsx
+++ src/layout/IntroduceCorp.jsx
@@ -1,8 +1,16 @@
+import layout from '../css/layout.css';
+import subImg from '../images/sub01_img.jpg';
+
 function IntroduceCorp() {
     return (
-        <div>
+        <div className="introduce_div">
+            <h1>ABOUT US</h1>
             <h2>Welcome to Chemsolution Co., Ltd.</h2>
-            <div>We have a lot of experiences and skills in chemical area. Our company believes in the customer-first policy, so we export and import the best quality chemical products. The products we deal with are isoparaffin, hydrocarbon, aromatic hydrocarbon, dearomatic hydrocarbon, naphthenic hydrocarbon and so on. We always do our best to supply our customers eco-friendly products that are safe for human and the environment.</div>
+            <p>We have a lot of experiences and skills in chemical area. Our company believes in the customer-first policy,<br/>
+               so we export and import the best quality chemical products. The products we deal with are isoparaffin, hydrocarbon,<br/>
+               aromatic hydrocarbon, dearomatic hydrocarbon, naphthenic hydrocarbon and so on. We always do our best to supply our<br/>
+               customers eco-friendly products that are safe for human and the environment.</p>
+            <img src={subImg} alt="chemsolution"></img>
         </div>
     )
 }
