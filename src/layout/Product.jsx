98e87c28f1fdf23bba46e404f5d9191d33f3f40d
Jimin Kim <ultimate1994@naver.com>
2022-02-19 16:12:09 +0900

    product 페이지에 beautify 설정

--- src/layout/Product.jsx
+++ src/layout/Product.jsx
@@ -1,124 +1,134 @@
 <div id="sub_cnt">
-		<div class="feedback">
-					<h3 class="h3_label">TOLUENE, XYLENE, #100, #150</h3>
-									<div class="process">
-				<span class="home">HOME</span>
-				<span class="arrow"><img src="http://chemsolution.net/theme/chemsolution/images/sub/arrow.png" alt=""/></span>
-				<span class="path">PRODUCTS</span>
-			
-								<span class="arrow"><img src="http://chemsolution.net/theme/chemsolution/images/sub/arrow.png" alt=""/></span>
-				<span class="path">AROMATIC SOLVENT</span>
-				<span class="arrow"><img src="http://chemsolution.net/theme/chemsolution/images/sub/arrow.png" alt=""/></span>
-				<span class="path">TOLUENE, XYLENE, #100, #150</span>
-			</div>	
-		</div>
-<article class="s0201 clear">
-	<div id="s0201_top">
-	 <p>CHEMSOL <b>Toluene</b> is a colorless transparent volatile liquid with aromatic odor and has a methyl group attached to benzene.<br/>
-			CHEMSOL <b>Xylene</b> is a colorless transparent volatile liquid with a directional odor and has two methyl groups attached to benzene. Xylene has three isomers, olso-, meta- and para-xylene, depending on the position of the methylene group</p>
-	</div>
-	<div class="s0201_name">
-		<ul>
-			<li>Typical Value</li>
-			<li></li>
-		</ul>
-	</div>
-	 <table class="table_2_1">
-		<thead>
-		<tr>
-			 <td class="bg">Properties</td>
-			 <td>Toluene</td>
-			 <td>Xylene</td>
-			 <td>#100</td>
-			 <td>#150</td>
-		</tr>
-		</thead>
-		<tbody><tr>
-			 <td class="bg">Appearance</td>
-			 <td>Liquid</td>
-			 <td>Liquid</td>
-			 <td>Liquid</td>
-			 <td>Liquid</td>
-		</tr>
-		<tr>
-			 <td class="bg">Odor</td>
-			 <td>benzene odor</td>
-			 <td>unique odorr</td>
-			 <td>-</td>
-			 <td>-</td>
-		</tr>
-		<tr>
-			 <td class="bg">Melting point /<br/>Freezing point</td>
-			 <td>-95℃</td>
-			 <td>-42℃</td>
-			 <td>-</td>
-			 <td>-</td>
-		</tr>
-		<tr>
-			<td class="bg">Initial Boiling Point /<br/>Boiling Ranges</td>
-			<td>110.6 ℃</td>
-			<td>137 ℃ ~ 143 ℃</td>
-			<td>155 ℃ ~ 180 ℃</td>
-			<td>170 ℃ ~ 210 ℃</td>
-		</tr>
-		<tr>
-			<td class="bg">Flash point</td>
-			<td>4 ℃ (c.c.)</td>
-			<td>32℃(o-), 27℃(m-),27℃(p-),<br/>Ethylbenzene 18℃</td>
-			<td>44.5℃</td>
-			<td>62℃</td>
-		</tr>
-		<tr>
-			<td class="bg">Evaporation rate</td>
-			<td>2.24</td>
-			<td>-</td>
-			<td>-</td>
-			<td>-</td>
-		</tr>
-		<tr>
-			<td class="bg">Specific graviti<br/>(Relative dnesity)</td>
-			<td>0.866~0.873</td>
-			<td>0.865~0.875 @15.56℃</td>
-			<td>0.86~0.88</td>
-			<td>0.89</td>
-		</tr>
-		<tr>
-			<td class="bg">Density</td>
-			<td>3.18</td>
-			<td>3.7</td>
-			<td>&gt;1.0</td>
-			<td>-</td>
-		</tr>
-		<tr>
-			<td class="bg">Viscosity</td>
-			<td>0.56</td>
-			<td>0.67</td>
-			<td>0.94</td>
-			<td>1.14</td>
-		</tr>
-	</tbody></table>
-	
-	<div class="features">
-		<div class="feat_t">
-			<span>Application</span>
-		</div>
-			<ul class="line1">
-				<li> Paint, Ink, Adhesive solvent, Resin solvent, TDI &amp; TNT Raw material, TNT, Audio tape coating</li>
-			</ul>
-			<ul class="line1">
-				<li> Used as a solvent for paint solvents and agricultural chemicals</li>
-			</ul>
-			<ul class="line1">
-				<li> Used as a raw material for olso-xylene (methophthalic acid), meta-xylene (isophthalic acid), paraxylene (terephthalic acid) and ethylbenzene <br/> (styrene monomer)</li>
-			</ul>
-			<ul class="line1">
-				<li> Industrial paint (ship, automobile) solvent, pesticide emulsifie</li>
-			</ul>
-		</div>
-</article>
+    <div class="feedback">
+        <h3 class="h3_label">TOLUENE, XYLENE, #100, #150</h3>
+        <div class="process">
+            <span class="home">HOME</span>
+            <span class="arrow"><img src="http://chemsolution.net/theme/chemsolution/images/sub/arrow.png"
+                    alt="" /></span>
+            <span class="path">PRODUCTS</span>
+
+            <span class="arrow"><img src="http://chemsolution.net/theme/chemsolution/images/sub/arrow.png"
+                    alt="" /></span>
+            <span class="path">AROMATIC SOLVENT</span>
+            <span class="arrow"><img src="http://chemsolution.net/theme/chemsolution/images/sub/arrow.png"
+                    alt="" /></span>
+            <span class="path">TOLUENE, XYLENE, #100, #150</span>
+        </div>
+    </div>
+    <article class="s0201 clear">
+        <div id="s0201_top">
+            <p>CHEMSOL <b>Toluene</b> is a colorless transparent volatile liquid with aromatic odor and has a methyl
+                group attached to benzene.<br />
+                CHEMSOL <b>Xylene</b> is a colorless transparent volatile liquid with a directional odor and has two
+                methyl groups attached to benzene. Xylene has three isomers, olso-, meta- and para-xylene, depending on
+                the position of the methylene group</p>
+        </div>
+        <div class="s0201_name">
+            <ul>
+                <li>Typical Value</li>
+                <li></li>
+            </ul>
+        </div>
+        <table class="table_2_1">
+            <thead>
+                <tr>
+                    <td class="bg">Properties</td>
+                    <td>Toluene</td>
+                    <td>Xylene</td>
+                    <td>#100</td>
+                    <td>#150</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="bg">Appearance</td>
+                    <td>Liquid</td>
+                    <td>Liquid</td>
+                    <td>Liquid</td>
+                    <td>Liquid</td>
+                </tr>
+                <tr>
+                    <td class="bg">Odor</td>
+                    <td>benzene odor</td>
+                    <td>unique odorr</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="bg">Melting point /<br />Freezing point</td>
+                    <td>-95℃</td>
+                    <td>-42℃</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="bg">Initial Boiling Point /<br />Boiling Ranges</td>
+                    <td>110.6 ℃</td>
+                    <td>137 ℃ ~ 143 ℃</td>
+                    <td>155 ℃ ~ 180 ℃</td>
+                    <td>170 ℃ ~ 210 ℃</td>
+                </tr>
+                <tr>
+                    <td class="bg">Flash point</td>
+                    <td>4 ℃ (c.c.)</td>
+                    <td>32℃(o-), 27℃(m-),27℃(p-),<br />Ethylbenzene 18℃</td>
+                    <td>44.5℃</td>
+                    <td>62℃</td>
+                </tr>
+                <tr>
+                    <td class="bg">Evaporation rate</td>
+                    <td>2.24</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="bg">Specific graviti<br />(Relative dnesity)</td>
+                    <td>0.866~0.873</td>
+                    <td>0.865~0.875 @15.56℃</td>
+                    <td>0.86~0.88</td>
+                    <td>0.89</td>
+                </tr>
+                <tr>
+                    <td class="bg">Density</td>
+                    <td>3.18</td>
+                    <td>3.7</td>
+                    <td>&gt;1.0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="bg">Viscosity</td>
+                    <td>0.56</td>
+                    <td>0.67</td>
+                    <td>0.94</td>
+                    <td>1.14</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <div class="features">
+            <div class="feat_t">
+                <span>Application</span>
+            </div>
+            <ul class="line1">
+                <li> Paint, Ink, Adhesive solvent, Resin solvent, TDI &amp; TNT Raw material, TNT, Audio tape coating
+                </li>
+            </ul>
+            <ul class="line1">
+                <li> Used as a solvent for paint solvents and agricultural chemicals</li>
+            </ul>
+            <ul class="line1">
+                <li> Used as a raw material for olso-xylene (methophthalic acid), meta-xylene (isophthalic acid),
+                    paraxylene (terephthalic acid) and ethylbenzene <br /> (styrene monomer)</li>
+            </ul>
+            <ul class="line1">
+                <li> Industrial paint (ship, automobile) solvent, pesticide emulsifie</li>
+            </ul>
+        </div>
+    </article>
 
 
 
 
 
-	</div>
+</div>
