2937c1bec1d69cb553b28115942dd7d35801ea8f
Jimin Kim <ultimate1994@naver.com>
2022-02-24 23:08:48 +0900

    Set link for a product.

---
+++ src/Components/product/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+
+function Navbar() {
+    return (
+        <Box>
+            <Typography>PRODUCTS</Typography>
+            <Link>AROMATIC SOLVENT</Link>
+            <Link>ALIPHATIC HYDROCARBON</Link>
+            <Link>PETROLEUM SOLVENT</Link>
+            <Link>LUBE BASE OIL</Link>
+            <Link>SYNTHETIC RESIN</Link>
+            <Link>ADDITIVE</Link>
+            <Link>LUBRICANT ADDITIVE</Link>
+            <Link>ELECTRONIC SOLVENT</Link>
+            <Link>ABBRASIVE</Link>
+        </Box>
+    )
+}
+
+export default Navbar;
