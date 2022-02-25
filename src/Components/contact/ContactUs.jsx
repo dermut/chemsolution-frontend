5bf34837bdbe24d971e3e4a7f758bc5cfd5335d5
Jimin Kim <ultimate1994@naver.com>
2022-02-25 22:10:36 +0900

    contact us 컴포넌트화

--- src/Components/contact/ContactUs.jsx
+++ src/Components/contact/ContactUs.jsx
@@ -1,66 +1,72 @@
-<div id="sub_layout">
-    <div id="ln_area">
-        <h2 class="h2_label">CONTACT US</h2>
-        <ul class="ln">
-            <li class="current"><a href="http://chemsolution.net/s3/s3_1.php">CONTACT US</a></li>
-        </ul>
-    </div>
-    <div id="sub_cnt">
-        <div class="feedback">
-            <h3 class="h3_label">CONTACT US</h3>
-            <div class="process">
-                <span class="home">HOME</span>
-                <span class="arrow"><img src="http://chemsolution.net/theme/chemsolution/images/sub/arrow.png" alt=""/></span>
-                <span class="path">CONTACT US</span>
-
+function ContactUs() {
+    return (
+        <div id="sub_layout">
+            <div id="ln_area">
+                <h2 class="h2_label">CONTACT US</h2>
+                <ul class="ln">
+                    <li class="current"><a href="http://chemsolution.net/s3/s3_1.php">CONTACT US</a></li>
+                </ul>
             </div>
-            <article class="s0301 clear">
+            <div id="sub_cnt">
+                <div class="feedback">
+                    <h3 class="h3_label">CONTACT US</h3>
+                    <div class="process">
+                        <span class="home">HOME</span>
+                        <span class="arrow"><img src="http://chemsolution.net/theme/chemsolution/images/sub/arrow.png" alt=""/></span>
+                        <span class="path">CONTACT US</span>
 
-                <div class="map_wrap" id="gg_map">
-                    <script
-                        src="https://maps.google.com/maps/api/js?key=AIzaSyC0T00zggiA9EDiEAYjfW8F_dFFf4Ui5JU&amp;language=en&amp;region=EN">
-                    </script>
+                    </div>
+                    <article class="s0301 clear">
 
-                    
+                        <div class="map_wrap" id="gg_map">
+                            <script
+                                src="https://maps.google.com/maps/api/js?key=AIzaSyC0T00zggiA9EDiEAYjfW8F_dFFf4Ui5JU&amp;language=en&amp;region=EN">
+                            </script>
 
+                            
+
+                        </div>
+                        <div class="s0301_map_text">
+                            <div class="mp_l">
+                                <img src="http://chemsolution.net/theme/chemsolution/images/sub/company_name.png"/>Company Name
+                            </div>
+                            <div class="mp_r">ChemSolution Co., Ltd.</div>
+                        </div>
+                        <div class="s0301_map_text">
+                            <div class="mp_l">
+                                <img src="http://chemsolution.net/theme/chemsolution/images/sub/address.png"/>Address(Zip Code)
+                            </div>
+                            <div class="mp_r">71-5, Haeryongsandan 5-ro, Haeryong-myeon, Suncheon-si, Jeollanam-do, Republic of
+                                Korea</div>
+                        </div>
+                        <div class="s0301_map_text">
+                            <div class="mp_l">
+                                <img src="http://chemsolution.net/theme/chemsolution/images/sub/tel.png"/>Tel</div>
+                            <div class="mp_r">82-61-726-8815</div>
+                        </div>
+                        <div class="s0301_map_text">
+                            <div class="mp_l">
+                                <img src="http://chemsolution.net/theme/chemsolution/images/sub/fax.png"/>Fax</div>
+                            <div class="mp_r">82-61-725-8814</div>
+                        </div>
+                        <div class="s0301_map_text">
+                            <div class="mp_l">
+                                <img src="http://chemsolution.net/theme/chemsolution/images/sub/e-mali.png"/>E-Mail</div>
+                            <div class="mp_r">chemsolution1004@naver.com</div>
+                        </div>
+                        <div class="s0301_map_text">
+                            <div class="mp_l over2">
+                                <img src="http://chemsolution.net/theme/chemsolution/images/sub/website.png"/>Website</div>
+                            <div class="mp_r over"><span>tradekorea</span>chemsolution.tradekorea.com
+                                <img src="http://chemsolution.net/theme/chemsolution/images/sub/dot.png"/><span>alibaba.com</span>kr1280696337blgv.trustpass.alibaba.com
+                            </div>
+                        </div>
+                        <div class="dot"></div>
+                    </article>
                 </div>
-                <div class="s0301_map_text">
-                    <div class="mp_l">
-                        <img src="http://chemsolution.net/theme/chemsolution/images/sub/company_name.png"/>Company Name
-                    </div>
-                    <div class="mp_r">ChemSolution Co., Ltd.</div>
-                </div>
-                <div class="s0301_map_text">
-                    <div class="mp_l">
-                        <img src="http://chemsolution.net/theme/chemsolution/images/sub/address.png"/>Address(Zip Code)
-                    </div>
-                    <div class="mp_r">71-5, Haeryongsandan 5-ro, Haeryong-myeon, Suncheon-si, Jeollanam-do, Republic of
-                        Korea</div>
-                </div>
-                <div class="s0301_map_text">
-                    <div class="mp_l">
-                        <img src="http://chemsolution.net/theme/chemsolution/images/sub/tel.png"/>Tel</div>
-                    <div class="mp_r">82-61-726-8815</div>
-                </div>
-                <div class="s0301_map_text">
-                    <div class="mp_l">
-                        <img src="http://chemsolution.net/theme/chemsolution/images/sub/fax.png"/>Fax</div>
-                    <div class="mp_r">82-61-725-8814</div>
-                </div>
-                <div class="s0301_map_text">
-                    <div class="mp_l">
-                        <img src="http://chemsolution.net/theme/chemsolution/images/sub/e-mali.png"/>E-Mail</div>
-                    <div class="mp_r">chemsolution1004@naver.com</div>
-                </div>
-                <div class="s0301_map_text">
-                    <div class="mp_l over2">
-                        <img src="http://chemsolution.net/theme/chemsolution/images/sub/website.png"/>Website</div>
-                    <div class="mp_r over"><span>tradekorea</span>chemsolution.tradekorea.com
-                        <img src="http://chemsolution.net/theme/chemsolution/images/sub/dot.png"/><span>alibaba.com</span>kr1280696337blgv.trustpass.alibaba.com
-                    </div>
-                </div>
-                <div class="dot"></div>
-            </article>
+            </div>
         </div>
-    </div>
-</div>
+    )
+}
+
+export default ContactUs;
