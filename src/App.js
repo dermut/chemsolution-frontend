0cadf88f965b6a31b26e18323045914c38b49a23
Jimin Kim <ultimate1994@naver.com>
2022-02-28 23:49:14 +0900

    add additional images

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HeaderNav from "./layout/HeaderNav";
 import Product from "./Components/product/Product";
 import MainPage from "./Components/mainPage/MainPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ContactUs from "./Components/contact/ContactUs";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Routes>
                     <Route path="" element={<MainPage />}></Route>
                     <Route path="/product" element={<Product />}></Route>
+                    <Route path="/contact" element={<ContactUs />}></Route>
                 </Routes>
             </BrowserRouter>
             <Footer></Footer>
