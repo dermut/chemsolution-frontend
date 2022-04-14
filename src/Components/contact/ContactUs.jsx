aa9cea1f511e21fb361bebba48034126c2e45c4f
ultimate1994 <ultimate1994@naver.com>
2022-04-14 21:49:32 +0900

    feat: add kakao map

--- src/Components/contact/ContactUs.jsx
+++ src/Components/contact/ContactUs.jsx
@@ -14,6 +14,34 @@ function ContactUs() {
         };
         
         let map = new kakao.maps.Map(container, options);
+
+        // 주소-좌표 변환 객체를 생성합니다
+        let geocoder = new kakao.maps.services.Geocoder();
+
+        // 주소로 좌표를 검색합니다
+        geocoder.addressSearch('해룡산단 5로 71-5', function(result, status) {
+
+            // 정상적으로 검색이 완료됐으면 
+            if (status === kakao.maps.services.Status.OK) {
+
+                var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                // 결과값으로 받은 위치를 마커로 표시합니다
+                var marker = new kakao.maps.Marker({
+                    map: map,
+                    position: coords
+                });
+
+                // 인포윈도우로 장소에 대한 설명을 표시합니다
+                var infowindow = new kakao.maps.InfoWindow({
+                    content: '<div style="width:150px;text-align:center;padding:6px 0;">켐솔루션</div>'
+                });
+                infowindow.open(map, marker);
+
+                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                map.setCenter(coords);
+            } 
+        });    
     }, []);
 
     return (
